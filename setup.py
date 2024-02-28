9b22efe896440ab66c25610eb2a9c6506a3ff9ef
zEpoch <1654306546@qq.com>
2024-02-28 15:53:32 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/28 16:09
+# @Author  : zhoutao
+# @File    : setup.py.py
+# @Software: VScode
+# @Email   : zhoutao3@genomics.cn
+
+import setuptools
+from wheel.bdist_wheel import bdist_wheel
+
+__version__ = "1.0.0"
+
+
+class BDistWheel(bdist_wheel):
+    def get_tag(self):
+        return (self.python_tag, "none", "any")
+
+
+cmdclass = {
+    "bdist_wheel": BDistWheel,
+}
  
-#这是一个和根目录相关的安装文件的列表，列表中setup.py更具体)
- 
-files = ["things/*"]
- 
-setup(
+
+requirements = open("requirements.txt").readline()
+
+setuptools.setup(
     name = "stTransfer",
     version = "1.0.0",
     description = "Transfer learning for spatial transcriptomics data and single-cell RNA-seq data.",
     author = "zhoutao",
     author_email = "zhotoa@foxmail.com",
-    url = "https://github.com/zepoch/stTransfer",
-    #Name the folder where your packages live:
-    #(If you have other packages (dirs) or modules (py files) then
-    #put them into the package directory - they will be found recursively.)
-    packages = ['package'],
-    #'package' package must contain files (see list above)
-    #I called the package 'package' thus cleverly confusing the whole issue...
-    #This dict maps the package name =to=> directories
-    #It says, package *needs* these files.
-    package_data = {'package' : files },
-    #'runner' is in the root.
-    scripts = ["runner"],
-    long_description = """Really long text here.""" 
-    #
-    #This next part it for the Cheese Shop, look a little down the page.
-    #classifiers = []     
+    url = "https://github.com/zepoch/stTransfer.git",
+    python_requires=">=3.10",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    cmdclass=cmdclass,
 )
